e72e8eb9ed9a7de8673758e5d0a5eda521aca704
Tristan Renaud <107555726+tristan-rnd@users.noreply.github.com>
2024-07-29 07:01:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+  return {
+    skills: {
+      js: 'Javascript',
+      ts: 'Typescript',
+      html: 'HTML',
+      css: 'CSS',
+      react: 'React',
+      vue: 'Vue',
+      angular: 'Angular',
+      node: 'Node',
+      express: 'Express',
+      nest: 'Nest',
+      mongo: 'MongoDB',
+      sql: 'SQL',
+      git: 'Git',
+      docker: 'Docker',
+      k8s: 'Kubernetes',
+      aws: 'AWS',
+      gcp: 'GCP',
+      azure: 'Azure',
+    },
+  };
+}
